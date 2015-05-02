7647fa1e09e24656c1b744a8a4af516e4afa3536
jaubourg <j@ubourg.net>
2015-05-02 09:45:40 +0200

    Initial commit. Little steps are still steps. Lots of work to do still.

---
+++ README.md
@@ -0,0 +1,29 @@
+[![NPM][npm-image]][npm-url]
+[![Build Status][travis-image]][travis-url]
+[![Coverage Status][coveralls-image]][coveralls-url]
+[![Dependency Status][dependency-image]][dependency-url]
+[![devDependency Status][devDependency-image]][devDependency-url]
+[![gratipay][gratipay-image]][gratipay-url]
+
+# jser
+
+a JSON-backed serializer support for nifty networking.
+
+## License
+
+Copyright (c) 2015 [Julian Aubourg](mailto:j@ubourg.net)
+
+Licensed under the [MIT license](https://raw.githubusercontent.com/jaubourg/jser/master/LICENSE-MIT).
+
+[coveralls-image]: https://img.shields.io/coveralls/jaubourg/jser.svg
+[coveralls-url]: https://coveralls.io/r/jaubourg/jser
+[dependency-image]: https://img.shields.io/david/jaubourg/jser.svg
+[dependency-url]: https://david-dm.org/jaubourg/jser
+[devDependency-image]: https://img.shields.io/david/dev/jaubourg/jser.svg
+[devDependency-url]: https://david-dm.org/jaubourg/jser#info=devDependencies
+[gratipay-image]: https://img.shields.io/gratipay/jaubourg.svg
+[gratipay-url]: https://gratipay.com/jaubourg/
+[npm-image]: https://img.shields.io/npm/v/jser.svg
+[npm-url]: https://npmjs.org/package/jser
+[travis-image]: https://img.shields.io/travis/jaubourg/jser.svg
+[travis-url]: https://travis-ci.org/jaubourg/jser
---
+++ test/jser.js
@@ -0,0 +1,111 @@
+"use strict";
+
+var Attempt = require( "attempt-js" );
+
+var jser = require( "../lib/jser" );
+
+module.exports = {
+	"no registered class": function( __ ) {
+		__.expect( 1 );
+		var serializer = jser( "@@jser@@" );
+		var object = {
+			"hello": "world"
+		};
+		var output = JSON.parse( serializer.request( object ) );
+		__.deepEqual( output, {
+			data: object,
+			objects: []
+		}, "no special treatment" );
+		__.done();
+	},
+	"registered class": function( __ ) {
+		__.expect( 1 );
+		var serializer = jser( "@@jser@@" );
+		serializer.register( {
+			name: "MyClass",
+			init: function( preString, flag ) {
+				this.string = preString + " world";
+				this.num = 666;
+				this.boolean = flag || false;
+			},
+			proto: {},
+			request: [ "boolean", "string" ]
+		} );
+		serializer.newInstance( "MyClass", "hello", 12 ).success( function( object ) {
+			var output = JSON.parse( serializer.request( object ) );
+			__.deepEqual( output, {
+				data: "@@jser@@0",
+				objects: [ {
+					"@@jser@@": "MyClass",
+					string: "hello world",
+					boolean: 12
+				} ]
+			}, "single object properly serialized" );
+		} ).always( function() {
+			__.done();
+		} );
+	},
+	"object within an object": function( __ ) {
+		__.expect( 1 );
+		var serializer = jser( "@@jser@@" );
+		serializer.register( {
+			name: "MyClass",
+			init: function( preString, flag ) {
+				this.string = preString + " world";
+				this.num = 666;
+				this.boolean = flag || false;
+			},
+			proto: {},
+			request: [ "boolean", "string" ]
+		} );
+		Attempt.join(
+			serializer.newInstance( "MyClass", "hello", 12 ),
+			serializer.newInstance( "MyClass", "bad" )
+		).success( function( object, child ) {
+			object.boolean = [ "a", child ];
+			var output = JSON.parse( serializer.request( object ) );
+			__.deepEqual( output, {
+				data: "@@jser@@0",
+				objects: [ {
+					"@@jser@@": "MyClass",
+					string: "hello world",
+					boolean: [ "a", "@@jser@@1" ]
+				}, {
+					"@@jser@@": "MyClass",
+					string: "bad world",
+					boolean: false
+				} ]
+			}, "object within an object properly serialized" );
+		} ).always( function() {
+			__.done();
+		} );
+	},
+	"recursive structure":  function( __ ) {
+		__.expect( 1 );
+		var serializer = jser( "@@jser@@" );
+		serializer.register( {
+			name: "MyClass",
+			init: function( preString, flag ) {
+				this.string = preString + " world";
+				this.num = 666;
+				this.boolean = flag || false;
+			},
+			proto: {},
+			request: [ "boolean", "string" ]
+		} );
+		serializer.newInstance( "MyClass", "hello", 12 ).success( function( object ) {
+				object.boolean = [ "a", object ];
+				var output = JSON.parse( serializer.request( object ) );
+				__.deepEqual( output, {
+					data: "@@jser@@0",
+					objects: [ {
+						"@@jser@@": "MyClass",
+						string: "hello world",
+						boolean: [ "a", "@@jser@@0" ]
+					} ]
+				}, "recursive structures properly serialized" );
+			} ).always( function() {
+				__.done();
+			} );
+	}
+};
