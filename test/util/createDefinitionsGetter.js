4541061778c3897ad3ae1571dfdd0255e17cfd73
jaubourg <j@ubourg.net>
2015-05-05 06:48:27 +0200

    Makes sure the definition getter is only called once

--- test/util/createDefinitionsGetter.js
+++ test/util/createDefinitionsGetter.js
@@ -1,9 +1,13 @@
 "use strict";
 
 module.exports = function( definitions ) {
+	var alreadyRequested = {};
 	return function( name ) {
 		if ( !definitions.hasOwnProperty( name ) ) {
 			throw "unknown class " + name;
+		}
+		if ( alreadyRequested.hasOwnProperty( name ) ) {
+			throw "class " + name + " was already requested earlier";
 		}
 		return definitions[ name ];
 	};
